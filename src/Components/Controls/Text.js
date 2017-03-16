c7084f3e72caa420b457ff18f1b88fa769a79de5
Nic <nwentling5@gmail.com>
2017-03-16 14:46:34 -0500

    Completed lecture

---
+++ src/Components/Controls/Text.js
@@ -0,0 +1,27 @@
+import React, {Component } from 'react';
+
+class Text extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: props.value
+        }
+    }
+
+    onChange(e){
+        this.setState({value: e.target.value}, function(){
+                // Callback function to send it back to App.js
+                this.props.onChange(this.state.value);
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <input className="form-control" type="number" value={this.state.value} onChange={this.onChange.bind(this)} />
+            </div>
+        )
+    }
+}
+
+export default Text;
